3e1a49ddd9deb8924f26d5b128b290e4061e4727
George2301 <george.ship.2000@gmail.com>
2023-04-21 12:22:55 +0300

    Updated API

--- API/api.py
+++ API/api.py
@@ -5,25 +5,43 @@ import numpy as np
 app = Flask(__name__)
 api = Api()
 
-model = keras.models.load_model('CNN_model.h5')
+model = keras.models.load_model('C:/Users/User/Desktop/Хакатон/hackathon0423/API/CNN.h5')
 
 @app.route('/api/model', methods=['POST'])
 def get(): 
     inp = request.get_json()
-    input = inp['input']
-    example = input_to_array(input)
-    prediction = model.predict(example, verbose=0)
-    prediction = np.where(prediction[0]==max(prediction[0]))[0][0]
+    input = inp
+    prediction = model.predict(input_prep_CNN(input), verbose=0)
+    prediction = np.where(prediction[0]==max(prediction[0]))[0][0]  
     return str(prediction)
     
-def input_to_array(input):
-    X = []
-    for line in input:
-        box = np.zeros((5))
-        for i in range(len(line)):
-            box[i] = float(line[i])
-        X.append(box)
-    return np.array([np.array(X)])
+def input_prep_CNN(item):
+    """
+    Creates tenzor for CNN model from initial json input from the Garpix dataset.
+    Takes dict as an input.
+    Returns numpy array as an output.
+    """
+    def prepare_item(item):
+        loading_size = item['data_result']['cargo_space']['loading_size']
+        norm_base = max(loading_size['width'], loading_size['height'], loading_size['length'])
+        for box in item['data_result']['boxes']:
+            box_size = box['size']
+            box['size_scale'] = {k: round(v*100/norm_base,2) for k,v in box_size.items()}
+            box['volume'] = box['size_scale']['width'] * box['size_scale']['height'] * box['size_scale']['length']
+        item['data_result']['boxes'] = sorted(item['data_result']['boxes'], key = lambda x: x['volume'], reverse = True)
+        item_new = dict()
+        item_new['loading_size'] = {k: round(v*100/norm_base,2) for k,v in loading_size.items()}
+        item_new['boxes'] = [{"size": box['size_scale'], "stacking": box['stacking'], "turnover": box['turnover']} for box in item['data_result']['boxes']]
+        return item_new
+    
+    def make_tenzor(item):
+        tenzoriezed_item = [[item['loading_size']['width'], item['loading_size']['height'], item['loading_size']['length'], False, False]] + \
+            [ [box['size']['width'], box['size']['height'], box['size']['length'], box['stacking'], box['turnover']] for box in item['boxes']]
+        add_dims = 2850 - len(tenzoriezed_item) #2850 - max boxes in the set
+        tenzoriezed_item = np.concatenate((np.array(tenzoriezed_item), np.zeros((add_dims, 5))))
+        return np.array([tenzoriezed_item])
+    
+    return make_tenzor(prepare_item(item))
 
 api.init_app(app)
 if __name__ == '__main__':
